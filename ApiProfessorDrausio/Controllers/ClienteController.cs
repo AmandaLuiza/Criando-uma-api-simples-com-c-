91968f4c7755abf6c378a8197a1bfe0e4f7f3bbc
Amanda Luiza <amandaluiza3225@gmail.com>
2022-06-22 09:40:46 -0300

    first commit

---
+++ ApiProfessorDrausio/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiProfessorDrausio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Minha primeira API .NET CORE no Framework 5.0";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Criando-uma-api-simples-com-c-
